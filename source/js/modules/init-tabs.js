34e29707ddb23e03daabb6c70dbd2d3af5bacc70
antonepifanov <antonepifanov87@gmail.com>
2022-11-28 16:21:13 +0300

    Создаёт ветку для деплоя

--- source/js/modules/init-tabs.js
+++
@@ -1,8 +0,0 @@
-import {Tabs} from '../vendor/tabs';
-
-export const getSubscriptionsTabs = () => {
-  return new Tabs('subscriptions', {
-    activeButtonClass: 'subscriptions__button--active',
-    activePanelClass: 'subscriptions__list--active',
-  });
-};
